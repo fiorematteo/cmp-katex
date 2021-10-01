edd25440bf07cb24ead218224209462e03e3951e
matteo <fiorematteo2002@gmail.com>
2021-10-01 13:44:47 +0200

    cleanup

--- lua/cmp_katex/init.lua
+++ lua/cmp_katex/init.lua
@@ -6,10 +6,6 @@ source.new = function()
   return self
 end
 
--- source.get_trigger_characters = function()
---   return { '\\' }
--- end
-
 source.complete = function(self, request, callback)
   if not self.items then
     self.items = require('cmp_katex.items')
