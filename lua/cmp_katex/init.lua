5d1f326cb610750e522d9c2c738a5b17ff6cc9e6
Xi Xia <againxx@mail.ustc.edu.cn>
2022-02-28 13:12:53 +0800

    Only triggered with \

--- lua/cmp_katex/init.lua
+++ lua/cmp_katex/init.lua
@@ -1,3 +1,4 @@
+local types = require('cmp.types')
 local source = {}
 
 source.new = function()
@@ -6,12 +7,20 @@ source.new = function()
   return self
 end
 
-source.complete = function(self, request, callback)
-  if not self.items then
-    self.items = require('cmp_katex.items')
-  end
+function source:get_trigger_characters()
+  return { "\\" }
+end
 
-  callback(self.items)
+
+function source:complete(params, callback)
+  if params.completion_context.triggerCharacter == '\\'
+    and params.completion_context.triggerKind == types.lsp.CompletionTriggerKind.TriggerCharacter then
+    if not self.items then
+      self.items = require('cmp_katex.items')
+    end
+
+    callback(self.items)
+  end
 end
 
 return source
